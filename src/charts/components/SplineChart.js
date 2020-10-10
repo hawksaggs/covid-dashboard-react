a40b9116a71ac32809490dd14aee7e6a7d2364fa
Ayush Mittal <ayush.mittal@outlook.com>
2020-10-10 18:30:14 +0530

    multiple chart integration

--- src/charts/components/SplineChart.js
+++ src/charts/components/SplineChart.js
@@ -5,18 +5,38 @@ const CanvasJS = CanvasJSReact.CanvasJS;
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const SplineChart = (props) => {
-    const [dataPoints, setDataPoints] = useState([]);
+    const [cases, setCases] = useState([]);
+    const [deaths, setDeaths] = useState([]);
+    const [recovered, setRecovered] = useState([]);
 
     const mapDataPoints = (props) => {
         console.log('SplineChart: ', props);
-        const dataPoints = (props.data && props.data.timeline) ? Object.keys(props.data.timeline.cases).map((key) => {
+        const cases = (props.data && props.data.timeline) ? Object.keys(props.data.timeline.cases).map((key) => {
             return {
                 x: new Date(key),
                 y: props.data.timeline.cases[key]
             };
         }) : [];
-        console.log('SplineChar | dataPoints: ', dataPoints);
-        setDataPoints(dataPoints);
+        console.log('SplineChar | cases: ', cases);
+        setCases(cases);
+
+        const deaths = (props.data && props.data.timeline) ? Object.keys(props.data.timeline.deaths).map((key) => {
+            return {
+                x: new Date(key),
+                y: props.data.timeline.deaths[key]
+            };
+        }) : [];
+        console.log('SplineChar | deaths: ', deaths);
+        setDeaths(deaths);
+
+        const recovered = (props.data && props.data.timeline) ? Object.keys(props.data.timeline.recovered).map((key) => {
+            return {
+                x: new Date(key),
+                y: props.data.timeline.recovered[key]
+            };
+        }) : [];
+        console.log('SplineChar | recovered: ', recovered);
+        setRecovered(recovered);
     };
 
     
@@ -36,12 +56,32 @@ const SplineChart = (props) => {
             title: "No of Cases",
             // prefix: "$"
         },
-        data: [{
-            yValueFormatString: "#,###",
-            xValueFormatString: "MMMM",
-            type: "spline",
-            dataPoints
-        }]
+        data: [
+            {
+                yValueFormatString: "#,###",
+                xValueFormatString: "MMMM",
+                type: "spline",
+                dataPoints: cases,
+                name:'cases',
+                showInLegend: true
+            },
+            {
+                yValueFormatString: "#,###",
+                xValueFormatString: "MMMM",
+                type: "spline",
+                dataPoints: deaths,
+                name:'deaths',
+                showInLegend: true
+            },
+            {
+                yValueFormatString: "#,###",
+                xValueFormatString: "MMMM",
+                type: "spline",
+                dataPoints: recovered,
+                name:'recovered',
+                showInLegend: true
+            }
+        ]
     };
     return (
         <div>
