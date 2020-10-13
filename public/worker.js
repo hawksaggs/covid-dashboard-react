9925503bda049dffa9c86e669dba3de11de3ebf2
Ayush Mittal <ayush.mittal@outlook.com>
2020-10-13 21:12:46 +0530

    change worker implementation

--- public/worker.js
+++ public/worker.js
@@ -1,18 +1,37 @@
 
-var CACHE_NAME = 'pwa-task-manager';
-var urlsToCache = [
-    '/',
-    '/completed'
+var VERSION = 'v1';
+var PREFIX = 'covid-cache';
+var CACHE_NAME = PREFIX + '-' + VERSION;
+var immutableRequests = [
+    '/images/*'
 ];
+
+var mutableRequests = [
+    '/index.html',
+    '/manifest.json'
+]
 
 // Install a service worker
 self.addEventListener('install', event => {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
+            .then(function (cache) {
+                var newImmutableRequests = [];
+                return Promise.all(
+                    immutableRequests.map(function (url) {
+                        return caches.match(url).then(function (response){
+                            if (response) {
+                                return cache.put(url, response);
+                            } else {
+                                newImmutableRequests.push(url);
+                                return Promise.resolve();
+                            }
+                        });
+                    })
+                ).then(function (){
+                   return cache.addAll(newImmutableRequests.concat(mutableRequests));
+                });
             })
     );
 });
@@ -27,19 +46,28 @@ self.addEventListener('fetch', event => {
                         return response;
                     }
                     return fetch(event.request);
-                }
-            )
+                })
+            .catch(function (){
+                return caches.match(event.request).then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    // else if (event.request.headers.get("accept").includes("text/html")) {
+                    //     return caches.match("/index-offline.html");
+                    // }
+                });
+            })
     );
 });
 
 // Update a service worker
 self.addEventListener('activate', event => {
-    var cacheWhitelist = ['pwa-task-manager'];
+    // var cacheWhitelist = [''];
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    if (CACHE_NAME !== cacheName &&  cacheName.startsWith(PREFIX)) {
                         return caches.delete(cacheName);
                     }
                 })
